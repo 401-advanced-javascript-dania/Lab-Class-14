a61ed2d996a7c0ceaa113389f7a0c673d8634c99
daniaalrababa9 <rababadania@gmail.com>
2020-02-15 21:34:59 +0200

    fix eslint erorr

--- acl-middleware.js
+++ acl-middleware.js
@@ -1,18 +1,18 @@
-'use strict';
+
 const users = require('./users.js');
 let aclMiddleware = function(ability){
-return (req,res,next)=>{
+  return (req,res,next)=>{
     try {
-        //to ckeck req.user.capabilites
-        //includes is array method return true or false
-        if(users.checkForcapability(ability,req.user.role)){
-            next();
-        }else{
-            next('access denied')
-        }
+      //to ckeck req.user.capabilites
+      //includes is array method return true or false
+      if(users.checkForcapability(ability,req.user.role)){
+        next();
+      }else{
+        next('access denied');
+      }
     }catch(err){
-        next('cannot login')
+      next('cannot login');
     }
-  }
-}
+  };
+};
 module.exports = aclMiddleware;
